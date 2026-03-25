8701064cdeb512ff5a4e949cdfa6db34364e01b7
hiupham13 <hieupham13012004@gmail.com>
2026-03-25 12:24:04 +0700

    up

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    process.env.FRONTEND_URL,
+    ...(process.env.FRONTEND_URL ? process.env.FRONTEND_URL.split(',') : [])
   ].filter(Boolean),
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
